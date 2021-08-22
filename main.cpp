9a1b0a792385b3f051cf462564dfebc2a391ae62
mmarszik <mmarszik@gmail.com>
2021-08-22 21:49:23 +0200

    .

--- main.cpp
+++ main.cpp
@@ -8,16 +8,19 @@ int main() {
     char buff_out[SIZE_OUT];
     char buff_inp[SIZE_INP];
     while( fread(buff_inp,SIZE_INP,1,stdin) == 1 ) {
+
         for( int i=0 ; i<SIZE_INP ; i++ ) {
-            if( buff_inp[i] != '0' && buff_inp[i] != '1') {
+            if( buff_inp[i] != '0' && buff_inp[i] != '1' ) {
                 abort();
             }
             buff_out[i/8] <<= 1;
             buff_out[i/8] |= buff_inp[i] == '1';
         }
+
         if( fwrite(buff_out,SIZE_OUT,1,stdout) != 1 ) {
-            abort();
+            break;
         }
+
     }
     return 0;
 }
